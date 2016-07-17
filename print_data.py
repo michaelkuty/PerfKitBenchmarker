9cf0cfca254b1d53a204515d20eacf3e9491e14e
Michael Kutý <6du1ro.n@gmail.com>
2016-07-17 14:42:41 +0200

    Add custom ordering to print data.

--- print_data.py
+++ print_data.py
@@ -2,7 +2,7 @@ import sys
 import json
 from prettytable import PrettyTable
 
-SKIPPEND_FIELDS = ['labels', 'sample_uri', 'official', 'run_uri', 'product_name', 'owner']
+SKIPPEND_FIELDS = ['sample_uri', 'official', 'run_uri', 'product_name', 'owner', 'timestamp']
 
 x = PrettyTable()
 
@@ -14,6 +14,16 @@ with open(sys.argv[1]) as data_file:
 
 keys = []
 
+keys_order = ['cloud', 'test', 'metric', 'value', 'unit']
+
+VALUE = {
+    'cloud': None,
+    'metric': None,
+    'value': None,
+    'test': None,
+    'unit': None,
+}
+
 for row in data['data']:
 
     row_values = []
@@ -23,13 +33,29 @@ for row in data['data']:
         if key in SKIPPEND_FIELDS:
             continue
 
-        if key not in keys:
-            keys.append(key)
+        if key == "labels":
+            if 'cloud' not in keys:
+                keys.append('cloud')
 
-        row_values.append(value)
+            values = value.split("|,|")
+            for key in values:
+                if 'cloud' in key:
+                    VALUE['cloud'] = key.split(':')[1]
+        else:
 
-    x.add_row(row_values)
+            if key not in keys:
+                keys.append(key)
 
-x._set_field_names(keys)
+            VALUE[key] = value
+
+    x.add_row(list([VALUE[key] for key in keys_order]))
+
+x._set_field_names(keys_order)
 
 print x
+
+name = sys.argv[1].split(".")[0]
+
+with open(name + ".txt", "w+") as new_file:
+
+    new_file.write(str(x))
